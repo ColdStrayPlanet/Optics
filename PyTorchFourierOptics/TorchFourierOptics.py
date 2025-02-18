44bfbe9e4c5205448d1730ab0b1cf7212d53a078
Richard A Frazin <polygraph911@gmail.com>
2025-02-18 14:45:29 -0500

    GPU implementation seems to work.  Both functions test_OpticalFT and test_fresnel_propagation seems to work as expected.

--- PyTorchFourierOptics/TorchFourierOptics.py
+++ PyTorchFourierOptics/TorchFourierOptics.py
@@ -169,21 +169,6 @@ class TorchFourierOptics:
        return gnew[0]
 
 
-   def _ResampleField2D(self, g, x, x_new):
-       if isinstance(x, np.ndarray):
-           x = torch.from_numpy(x).float()
-       if isinstance(x_new, np.ndarray):
-           x_new = torch.from_numpy(x_new).float()
-
-       x_torch = (2 * x_new - x.max() - x.min()) / (x.max() - x.min())
-       grid_x, grid_y = torch.meshgrid(x_torch, x_torch, indexing='xy')
-       grid = torch.stack((grid_x, grid_y), dim=-1)  # (len(x_new), len(x_new), 2)
-       # Ajustar las dimensiones de `g` para `grid_sample`
-       g = g.unsqueeze(0)  # Añadir dimensión de lote: (1, 2, N, N)
-       # Unsqueeze en grid si es necesario para asegurar que batch_size es 1
-       grid = grid.unsqueeze(0)  # Añadir dimensión de lote: (1, len(x_new), len(x_new), 2)
-       gnew = torch.nn.functional.grid_sample(g, grid, mode='bicubic', padding_mode='zeros', align_corners=False)
-       return gnew[0]
 
    """
     Simula una óptica perfecta de transformada de Fourier usando la propagación FFT.
@@ -259,43 +244,6 @@ class TorchFourierOptics:
        f = self.ResampleField2D(ff, fft_grid, x_out)
        return f
 
-   def _FFT_prop(self, g, x, x_out, focal_length):
-         M = len(x)
-         assert g[0].shape == (M,M)
-         wavelength = self.params['wavelength']
-         tru_freq_spacing = (x_out[1]-x_out[0])/(focal_length*wavelength) # physical spatial frequency spacing
-         #figure out the amount of padding needed for the FFT to capture the physical spatial frequencies
-         MP = 32  # MP is the total number of points in the padded array (1D)
-         while True:
-            P = MP - M  #  P is the number of zero-pad values
-            if P < 0:
-               MP *=2
-               continue
-            L_pad = (MP/M)*(x.max() - x.min())
-            FFT_spacing = 1/L_pad
-            if tru_freq_spacing/FFT_spacing >=2:
-               break
-            else: MP *= 2
-
-         g_pad = torch.zeros((2,MP,MP))  # create padded array
-         k1 = MP//2 - M//2;  k2 = k1 + M
-         g_pad[0, k1:k2, k1:k2] = g[0]
-         g_pad[1, k1:k2, k1:k2] = g[1]
-         g_pad = torch.complex(g_pad[0],g_pad[1])
-         f_complex = torch.fft.fftshift(torch.fft.fft2(torch.fft.ifftshift(g_pad)))
-         ff = torch.stack((torch.real(f_complex), torch.imag(f_complex)),dim=0)
-         if np.remainder(MP,2) == 0:
-            fft_grid = torch.linspace(-MP//2,MP//2-1,MP)*FFT_spacing
-         else:
-            fft_grid = torch.linspace(-MP//2,MP//2  ,MP)*FFT_spacing
-         #scale the spatial frequency grid back to (output plane) distance units.
-         #Note the conversion of fft_grid back to spatial coordinates refers to the input plane, not the output plane.
-         #  So, if you are focusing a collimated beam you could have fft_grid be much wider than x_out.  This is OK.
-         fft_grid *= focal_length*wavelength
-
-         f = self.ResampleField2D(ff, fft_grid , x_out)
-         return f
-
 
     #2D Fresenel prop using convolution in the spatial domain
     #NOTE: This routine zeros the Fresnel kernel at distances beyond which the
@@ -335,16 +283,12 @@ class TorchFourierOptics:
            raise ValueError("Input field and grid must have the same sampling.")
        if g.shape[1] != g.shape[2]:
            raise ValueError("Input field array must be square.")
-       if not np.isscalar(length_out):
-           raise ValueError("length_out parameter must be a scalar.")
-
 
        # Ensure all tensors are on the correct device
        g = g.to(self.device)
        x = x.to(self.device)
 
        lam = self.params['wavelength'] / index_of_refraction
-       dx = x[1] - x[0]
        length_input = x.max() - x.min()
        dx_chirp = (self.params['max_chirp_step_deg'] / 180) * lam * z / (length_input + length_out)
 
@@ -364,12 +308,11 @@ class TorchFourierOptics:
        # Resample input field on a grid with spacing dx_new
        nx_new = (length_input / dx_new).floor().to(torch.int)
        x_new = torch.linspace(-length_input / 2 + dx_new / 2, length_input / 2 - dx_new / 2, nx_new, device=self.device)
-       g, x = self.ResampleField2D(g, x, x_new)
-       dx = x[1] - x[0]
+       g = self.ResampleField2D(g, x, x_new)
 
-       ns = torch.round( (length_input + length_out)/dx ).floor().to(torch.int)
+       ns = torch.round( (length_input + length_out)/dx_new ).floor().to(torch.int)
        ns = int(ns.item())
-       s = torch.linspace(-length_input/2 - length_out/2 + dx/2, length_input/2 + length_out/2 - dx/2, ns, device=self.device)
+       s = torch.linspace(-length_input/2 - length_out/2 + dx_new/2, length_input/2 + length_out/2 - dx_new/2, ns, device=self.device)
        sx, sy = torch.meshgrid(s, s, indexing='xy')
 
        kern = torch.exp(1j * torch.pi * (sx**2 + sy**2) / (lam * z))
@@ -379,7 +322,7 @@ class TorchFourierOptics:
 
        # Apply quadratic cutoff
        r = torch.sqrt(sx**2 + sy**2)
-       s_max = lam * z * self.params['max_chirp_step_deg'] / (360 * dx)
+       s_max = lam * z * self.params['max_chirp_step_deg'] / (360 * dx_new)
        d_small = 1. / kernphase_deriv(s_max)  # Small distance beyond which the kernel drops to zero
        cutoff = torch.ones_like(r)
        # No cutoff for r < s_max
@@ -423,99 +366,6 @@ class TorchFourierOptics:
 
 
 
-
-
-   def _ConvFresnel2D(self, g, x, length_out, z,  set_dx=True, index_of_refraction=1):
-        if g.shape[1] != x.shape[0]:
-            raise ValueError("Input field and grid must have the same sampling.")
-        if g.shape[1] != g.shape[2]:
-            raise ValueError("Input field array must be square.")
-
-        lam = self.params['wavelength'] / index_of_refraction
-        dx = x[1] - x[0];  length = x.max() - x.min()
-        dx_chirp = (self.params['max_chirp_step_deg'] / 180) * lam * z / (length_input + length_out)
-
-        if isinstance(set_dx, bool):
-            if set_dx == False:
-                dx_new = x[1] - x[0]
-            else:  # set_x is True. Use chirp sampling criterion
-                    dx_new = dx_chirp
-        else:  # set_dx is not a bool. Take dx_new to be value of set_dx
-            if not isinstance(set_dx, float):
-                raise Exception("ConvFresnel2D: set_dx must be a bool or a float.")
-            if set_dx <= 0:
-                raise Exception("ConvFresnel2D: numerical value of set_dx must be > 0.")
-            dx_new = set_dx
-
-        # Resample input field on a grid with spacing dx_new
-        nx_new = int(length_input / dx_new)
-        x_new = np.linspace(-length_input/2 + dx_new/2, length_input/2 - dx_new/2, nx_new)
-        g, x = self.ResampleField2D(g, x, x_new)
-        dx = x[1] - x[0]
-
-        ns = int(torch.round((length_input + length_out) / dx))
-        s = torch.linspace(-length_input / 2 - length_out / 2 + dx / 2, length_input / 2 + length_out / 2 - dx / 2, ns)
-        sx, sy = torch.meshgrid(s, s, indexing='xy')
-        kern = torch.exp(1j * torch.pi * (sx**2 + sy**2) / (lam*z))
-        kern /= torch.sum(torch.abs(kern))  #apply normalization
-        kernphase_deriv = lambda r: 2*torch.pi*r/(lam*z)  # radial gradient of the phase of the chirp kernel
-
-        # Apply quadratic cutoff
-        r = torch.sqrt(sx**2 + sy**2)
-        s_max = lam * z * self.params['max_chirp_step_deg'] / (360 * dx)
-        d_small = 1./kernphase_deriv(s_max)  # Small distance beyond which the kernel drops to zero
-        cutoff = torch.ones_like(r)
-        # No cutoff for r < s_max
-        cutoff[r > s_max] = 1 - (r[r > s_max] - s_max) ** 2 / (d_small ** 2)
-        cutoff[cutoff < 0] = 0  # Ensure that the cutoff doesn't go negative
-        kern *= cutoff
-
-        # Aplicar padding a `g` para igualar el tamaño de `kern`.
-#        pad_size = [((kern.size(0) - g.size(1)) + 1) // 2, ((kern.size(1) - g.size(2)) + 1) // 2]
-#        g_padded_real = torch.nn.functional.pad(g[0], pad=pad_size)
-#        g_padded_imag = torch.nn.functional.pad(g[1], pad=pad_size)
-
-
-        # Aplicar padding a `g` para igualar el tamaño de `kern`.
-        pad_y = (kern.size(0) - g.size(1)) // 2
-        pad_x = (kern.size(1) - g.size(2)) // 2
-        # El padding se hace simétrico
-        g_padded_real = torch.nn.functional.pad(g[0], (pad_x, pad_x, pad_y, pad_y))
-        g_padded_imag = torch.nn.functional.pad(g[1], (pad_x, pad_x, pad_y, pad_y))
-
-        # Separar la parte real e imaginaria del kernel
-        kern_real_shifted = torch.fft.fftshift(kern.real)
-        kern_imag_shifted = torch.fft.fftshift(kern.imag)
-
-        #print("Tamaño de g (padded real):", g_padded_real.size())
-        #print("Tamaño de g (padded imag):", g_padded_imag.size())
-        #print("Tamaño de kern (real):", kern_real_shifted.size())
-        #print("Tamaño de kern (imag):", kern_imag_shifted.size())
-        #return None
-
-
-        # Aplicar fftshift en la señal g paddeada
-        g_real_shifted = torch.fft.fftshift(g_padded_real)
-        g_imag_shifted = torch.fft.fftshift(g_padded_imag)
-
-        # Realizar transformadas de Fourier en las partes reales e imaginarias
-        G_real = torch.fft.fft2(g_real_shifted)
-        G_imag = torch.fft.fft2(g_imag_shifted)
-        K_real = torch.fft.fft2(kern_real_shifted)
-        K_imag = torch.fft.fft2(kern_imag_shifted)
-
-        # Calcular la convolución en el dominio de la frecuencia
-        H_real_shifted = G_real * K_real - G_imag * K_imag
-        H_imag_shifted = G_imag * K_real + G_real * K_imag
-
-        # Aplicar la transformada inversa de Fourier
-        h_real = torch.fft.ifftshift(torch.fft.ifft2(H_real_shifted).real)
-        h_imag = torch.fft.ifftshift(torch.fft.ifft2(H_imag_shifted).real)
-
-        # Reconstruir la señal resultante compleja
-        h = torch.stack((h_real, h_imag), dim=0)
-
-        return (h / (lam * z), s)
 
     #Apply the thin lens phase transformation
     #g - two channel input field (each channel is 2D)
@@ -593,60 +443,6 @@ class TorchFourierOptics:
 
        return h
 
-
-
-   def _ApplyThinLens2D(self, g, x, set_dx, focal_length, center=[0,0]):
-          if g.size(1) != x.size(0):
-              raise ValueError("El campo de entrada y las coordenadas deben tener el mismo tamaño.")
-          wavelength = self.params['wavelength']
-          philim = self.params['max_chirp_step_deg']*torch.pi/180
-          max_step = philim*wavelength*focal_length/(2*torch.pi*x.max())
-          center_x, center_y = center
-
-          if isinstance(set_dx, bool):
-               if set_dx == False:
-                   dx_new = x[1] - x[0]
-                   if dx_new > max_step:
-                      print("ApplyThinLens2D: You chose set_dx to be False, but the current sampling is too coarse.")
-                      print("max dx =",max_step,"current dx =", dx_new)
-                      raise Exception()
-               else:  # set_x is True. Use chirp sampling criterion
-                       dx_new = max_step
-          else:  # set_dx is not a bool. Take dx_new to be value of set_dx
-               if not isinstance(set_dx, float):
-                   raise Exception("ApplyThinLens2D: set_dx must be a bool or a float.")
-               if set_dx <= 0:
-                   raise Exception("ApplyThinsLens2D: numerical value of set_dx must be > 0.")
-               dx_new = set_dx
-               if dx_new > max_step:
-                      print("ApplyThinLens2D: You chose set_dx to be,", set_dx ," but that is too coarse.  max dx =",max_step)
-                      raise Exception()
-
-          x_new = torch.linspace(x.min(),x.max(), int( (x.max()-x.min())/dx_new))
-          g = self.ResampleField2D(g,x,x_new)
-
-          X, Y = torch.meshgrid(x_new - center_x, x_new - center_y, indexing='xy')
-          phase_factor = torch.exp(-1j * torch.pi * (X**2 + Y**2) / (wavelength*focal_length))
-
-          # Separar canales real e imaginario de g
-          g_real = g[0]
-          g_imag = g[1]
-
-          # Crear el campo complejo a partir de sus componentes reales e imaginarias
-          g_complex = torch.complex(g_real, g_imag)
-
-          # Aplicar la fase del lente
-          g_transformed = g_complex*phase_factor
-
-          # Dividir el campo resultado en sus partes real e imaginaria
-          g_transformed_real = g_transformed.real
-          g_transformed_imag = g_transformed.imag
-
-          # Reconstruir la señal resultante en forma de tensor de dos canales
-          h = torch.stack((g_transformed_real, g_transformed_imag), dim=0)
-
-          return h
-
    """
         This applies a centered stop and/or an aperture to the field.
         In the clear zone,   it multiplies the field by unity.
@@ -722,30 +518,33 @@ def test_OpticalFT():  # take an optical FT of a disk
    d_circ = 20.e3  # circle diameter (microns)
    focal_length = 1.e6  # (microns)
 
-   optics = TorchFourierOptics(params={'wavelength': 0.9, 'max_chirp_step_deg':30.0})
-   x_in = torch.linspace(-w/2 + w/(2*N), w/2 - w/(2*N), N)  # input grid (1D)
-   lam = optics.params['wavelength']
+   Optics = TorchFourierOptics(params={'wavelength': 0.9, 'max_chirp_step_deg':30.0})
+   x_in = torch.linspace(-w/2 + w/(2*N), w/2 - w/(2*N), N).to(Optics.device)  # input grid (1D)
+   lam = Optics.params['wavelength']
    fld = focal_length*lam/d_circ  # "lambda/D" scaled by the focal length
    d_out = 40*fld;  dx_out = fld/3; N_out = int(d_out//dx_out)
-   x_out = torch.linspace(-d_out/2 + d_out/(2*N_out), d_out/2 - d_out/(2*N_out) ,N_out)
+   x_out = torch.linspace(-d_out/2 + d_out/(2*N_out), d_out/2 - d_out/(2*N_out) ,N_out).to(Optics.device)
 
-   X,Y = torch.meshgrid(x_in, x_in, indexing='ij')
-   R = torch.sqrt(X**2 + Y**2)
-   circ = torch.ones_like(Y)
+   X,Y = torch.meshgrid(x_in, x_in, indexing='ij')  # OK on cpu
+   R = torch.sqrt(X**2 + Y**2)  # OK on cpu
+   circ = torch.ones_like(Y).to(Optics.device)
    circ[R > d_circ/2] = 0.
 
    #specify two-channel input field
    g_in = torch.stack((circ, torch.zeros_like(circ)), axis=0)
    # take optical FT
-   g = optics.FFT_prop(g_in, x_in, x_out, focal_length)
+   g = Optics.FFT_prop(g_in, x_in, x_out, focal_length)
 
-   output_field_np = optics.torch2numpy_complex(g)
+   output_field_np = Optics.torch2numpy_complex(g)
+   x_out_np = x_out.cpu().numpy()
+
+   plt.figure(); plt.plot(x_out_np, np.log10(1.+ np.abs( output_field_np[:,output_field_np.shape[0]//2])), 'ko-');
 
    plt.figure();
-   plt.imshow(np.abs(output_field_np), extent=(x_out[0].item(), x_out[-1].item(), x_out[0].item(), x_out[-1].item()))
-   plt.title('Campo de salida (propagado)')
-   plt.colorbar()
-   plt.show()
+   extent = (x_out[0].item(), x_out[-1].item(), x_out[0].item(), x_out[-1].item())
+   plt.imshow(np.log10(np.abs(output_field_np) + 10.), extent=extent)
+   plt.title('|Campo| de salida (propagado)')
+   plt.colorbar();
 
 
    return None
@@ -761,18 +560,18 @@ def test_fresnel_propagation():
     wavelength = 0.9  # Longitud de onda en micrómetros
     z = 1000.  # Distancia de propagación en micrómetros
     params = {'wavelength': wavelength, 'max_chirp_step_deg': 30.0}
-    optics = TorchFourierOptics(params=params)
+    Optics = TorchFourierOptics(params=params)
 
     # Crear un campo de entrada cuadrado uniforme (por simplicidad)
     input_field = np.ones((N, N), dtype=np.complex64)
     # Convertir el campo de entrada en un tensor de PyTorch complejo (2, N, N)
-    g = optics.numpy_complex2torch(input_field)
+    g = Optics.numpy_complex2torch(input_field)
 
-    #ConvFresnel2D(self, g, x, length_out, z, set_dx=True, index_of_refraction=1)
-    output_field, s = optics.ConvFresnel2D(g, x, L, z)
+    #ConvFresnel2D(self, g, x, length_out, z, set_dx=True, index_of_refraction=1)  #  from the function definition
+    output_field, s = Optics.ConvFresnel2D(g, x, L, z)
 
     # Convertir el resultado a un array NumPy complejo
-    output_field_np = optics.torch2numpy_complex(output_field)
+    output_field_np = Optics.torch2numpy_complex(output_field)
 
     # Mostrar el campo de entrada y el campo de salida
     plt.figure(figsize=(12, 5))
