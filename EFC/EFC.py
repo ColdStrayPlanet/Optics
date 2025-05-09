6c791ee58e8dddc52e305408b30731e04df3c4f3
Richard A Frazin <polygraph911@gmail.com>
2025-05-09 18:47:43 -0400

    ...

--- EFC/EFC.py
+++ EFC/EFC.py
@@ -10,7 +10,8 @@ import numpy as np
 from os import path as ospath  #needed for isfile(), join(), etc.
 from sys import path as syspath
 import warnings
-from scipy import optimize
+from scipy.optimize import minimize, LinearConstraint
+from scipy.sparse import lil_matrix
 
 machine = "homeLinux"
 #machine = "officeWindows"
@@ -43,6 +44,7 @@ Delta11 = 1./11; Delta15 = 1./15; Delta21=1./21; Delta33 = 1./31  # setting delt
 B21 = BS.BivariateCubicSpline(xx.flatten(),yy.flatten(),21,Xmin=-0.5+Delta21,Delta=Delta21)
 B33 = BS.BivariateCubicSpline(xx.flatten(),yy.flatten(),33,Xmin=-0.5-Delta33,Delta=Delta33)
 
+#%%
 
 class EFC():
    def __init__(self, SpLo, SpHi, DomPropMat, CrossPropMat, Cross2PropMat=None):
@@ -51,11 +53,11 @@ class EFC():
       self.SysD = DomPropMat
       self.SysC = CrossPropMat
       self.SysC2 = Cross2PropMat
+      self.detshape = (int(np.sqrt(DomPropMat.shape[0])), int(np.sqrt(DomPropMat.shape[0])))   # shape of detector
       if DomPropMat.shape != CrossPropMat.shape: raise ValueError("DomPropMat and CrossPropMat must have the same shape.")
       if (Cross2PropMat is not None) and (): raise ValueError("The two CrossProp matrices must have the same shape.")
       if SpHi.Nsp != DomPropMat.shape[1]: raise ValueError("The hi-res spline and the propagation matrix must be consistent.")
       return None
-
 
    #this takes a DM command on the low-res spline grid and calculates the the spline coefs
    #  on the hi-res grid.
@@ -71,21 +73,23 @@ class EFC():
          dphase = self.SpLo.mat
       phasor = np.exp(1j*phase)
       if return_grad:
-         dphasor = 1j*dphase*phasor
-      HiCo = self.SpHi.GetSplCoefs(phasor)
+         dphasor = 1j*(dphase.T*phasor).T
+      HiCo = self.SpHi.imat@phasor
       if return_grad:
-         dHiCo = self.SpHi.GetSplCoefs(dphasor)
+         dHiCo = self.SpHi.imat@dphasor
          return( (HiCo, dHiCo) )
       return(None)
 
 
-   def DMcmd2DetField(self, dmc, pmat='dom', return_grad=False):
+   def DMcmd2DetField(self, dmc, pmat='dom', pixlist=None, return_grad=False):
       if pmat not in ['dom','cross','cross2']:
          raise ValueError("the string pmat must be one of the allowable choices.")
       if pmat == 'cross2' and self.C2 is None: raise Exception("Cross2PropMat not initialized.")
       if pmat == 'dom': Sys = self.SysD
       if pmat == 'cross': Sys = self.SysC
       if pmat == 'cross2': Sys = self.SysC2
+      if pixlist is not None:
+         Sys = Sys[pixlist,:]
       if not return_grad:
          co = self.DMcmd2PupilCoefs(dmc)
          field = Sys@co
@@ -94,4 +98,69 @@ class EFC():
          co, dco = self.DMcmd2PupilCoefs(dmc, return_grad=True)
          field = Sys@co
          jac = Sys@dco
-         return ( (field, jac))
+         return ( (field, jac) )
+
+   def DMcmd2Intensity(self, dmc, pmat='dom', pixlist=None, return_grad=False):
+      if not return_grad:
+         field = self.DMcmd2Field(dmc, pmat, pixlist=pixlist, return_grad=False)
+         I = field*np.conj(field)
+         return(I)
+      else:
+         field, dfield = self.DMcmd2DetField(dmc, pmat, pixlist=pixlist, return_grad=True)
+         I = field*np.conj(field)
+         dI = 2*np.real( (dfield.T)*np.conj(field)  ).T
+         return( (I, dI) )
+
+   #This makes a list of the 1D pixel indices corresponding to a rectangular
+   # region (specified by its corners) within a flattened 2D array.
+   #corners - list (or array) corresponding to the 2D coords of the corners of the
+   # desired region in the following order [Xmin, Xmax, Ymin, Ymax].  The boundaries
+   # are inclusive.
+   #BigArrayShape a tuple (or whatever) corresponding the shape of the larger array
+   def MakePixList(self, corners):
+       BigArrayShape=self.detshape
+       assert corners[2] <= BigArrayShape[0] and corners[3] <= BigArrayShape[0]
+       assert corners[0] <= BigArrayShape[1] and corners[2] <= BigArrayShape[1]
+       pixlist = []
+       rows = np.arange(corners[2], corners[3]+1)  # 'y'
+       cols = np.arange(corners[0], corners[1]+1)  # 'x'
+       ff = lambda r,c : np.ravel_multi_index((r,c), (BigArrayShape[0],BigArrayShape[1]))
+       for r in rows:
+           for c in cols:
+               pixlist.append(ff(r,c))
+       return pixlist
+
+   def DigDominantHole(self, dmc, pixlist, DMconstr=np.pi/4):
+      N = int(np.sqrt(self.SpHi.Nsp))
+      n = N*N
+      def make_gradient_matrix(N):  # this for the inequality constraints on DM command
+          rows = []
+          cols = []
+          data = []
+          idx = lambda i, j: i * N + j  # 1D index of actuator (i,j)
+          row_id = 0
+          for i in range(N):  # ∂q/∂x (horizontal)
+              for j in range(N - 1):
+                  rows += [row_id, row_id]
+                  cols += [idx(i, j), idx(i, j + 1)]
+                  data += [-1, 1]
+                  row_id += 1
+          for i in range(N - 1):  # ∂q/∂y (vertical)
+              for j in range(N):
+                  rows += [row_id, row_id]
+                  cols += [idx(i, j), idx(i + 1, j)]
+                  data += [-1, 1]
+                  row_id += 1
+
+          A = lil_matrix((row_id, N * N))
+          for r, c, d in zip(rows, cols, data):
+              A[r, c] = d
+          return A.tocsr()
+      A = make_gradient_matrix(N)
+      constraint = LinearConstraint(A, -DMconstr, DMconstr)
+
+      f = lambda dmc : self.DMcmd2Intensity(dmc, pmat='dom', pixlist=pixlist, return_grad=True)
+      ops = {'maxiter':50, 'xtol':1.e-6, 'verbose':2}
+      out = minimize(f, dmc, jac=True, constraints=[constraint], method='trust-constr',options=ops)
+
+      return out
