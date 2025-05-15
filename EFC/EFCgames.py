c23db0544eea562617a4ebb7abc42d434a17c688
Richard A Frazin <polygraph911@gmail.com>
2025-05-15 19:03:22 -0400

    ...

--- EFC/EFCgames.py
+++ EFC/EFCgames.py
@@ -11,6 +11,13 @@ import matplotlib.pyplot as plt
 import pickle
 import EFC
 
-with open('Stuff05152025.pickle','rb') as fp: stuff = pickle.load(fp)
+with open('Stuff05152025.pickle','rb') as fp: stuff = pickle.load(fp); del(fp)
+
 
 A = EFC.EFC(EFC.B21, EFC.B33, EFC.DomMat, EFC.CroMat)
+
+
+
+
+
+plt.figure(); plt.plot(np.log10(stuff['cost45'])-2.35,'ko-',np.log10(stuff['costXax'])-2.35,'bo-');
