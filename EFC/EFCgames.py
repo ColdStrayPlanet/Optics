9706c7d950e6e7b420296b7cf409a8a6bfb6768e
Richard A Frazin <polygraph911@gmail.com>
2025-05-19 20:37:48 -0400

    ...

--- EFC/EFCgames.py
+++ EFC/EFCgames.py
@@ -19,26 +19,61 @@ cost45  = stuff['cost45']  # objective fcn values
 costAx  = stuff['costXax']
 pl45 =  stuff['pixlist45d']  # list of dark hole pixels
 plXax = stuff['pixlistXax']
+IoffAx45 = stuff['IoffAx45']  #intensity of off-axis beam
+IoffAxXax = stuff['IoffAxXax']
+CostCross45 = stuff['CostCross45'] # sum of cross intensity in dark hole
+CostCrossXax = stuff['CostCrossXax']
+sol45 = sols45[-1] # final dark hole solution
+solXax = solsXax[-1]
 
 PhasorXax = A.LinearPhaseForOffAxisSource(5.2, 0. , output='Phasor')
 Phasor45  = A.LinearPhaseForOffAxisSource(7.0, 45., output='Phasor')
+#%%
+#functions for modulated intensities
+Cd45 = lambda cmd: np.sum(A.DMcmd2Intensity(sol45+cmd, 'dom', pixlist=pl45 ))
+Cc45 = lambda cmd: np.sum(A.DMcmd2Intensity(sol45+cmd, 'cross', pixlist=pl45 ))
+CdXax = lambda cmd: np.sum(A.DMcmd2Intensity(solXax+cmd, 'dom', pixlist=plXax ))
+CcXax = lambda cmd: np.sum(A.DMcmd2Intensity(solXax+cmd, 'cross', pixlist=plXax ))
+Ioa45  = lambda cmd: np.max(A.DMcmd2Intensity(sol45  + cmd, pmat='dom' ,pixlist=pl45, OffAxPhasor=Phasor45))
+IoaXax = lambda cmd: np.max(A.DMcmd2Intensity(solXax + cmd ,pmat='dom',pixlist=plXax,OffAxPhasor=PhasorXax))
 
-CostCross45 = [];  CostCrossXax = []  # total cross intensities in dark hole
-IoffAxXax = []; IoffAx45  = [] # peak off-axis intensities
-
-
-for k in range(len(sols45)):
-   CostCross45.append( np.sum(A.DMcmd2Intensity(sols45[k ], 'cross', pixlist=pl45 )))
-   IoffAx45.append(np.max(A.DMcmd2Intensity(sols45[k], pmat='dom' ,pixlist=pl45, OffAxPhasor=Phasor45)))
-for k in range(len(solsXax)):
-   CostCrossXax.append(np.sum(A.DMcmd2Intensity(solsXax[k], 'cross', pixlist=plXax)))
-   IoffAxXax.append(np.max(A.CMcmd2Intensity(solsXax[k],pmat='dom',pixlist=plXax,OffAxPhasor=PhasorXax)))
+#lambda functions for random modulation
+cmddom = lambda  :  0.002*np.pi*(np.random.rand(len(sol45)) - 0.5)
+cmdcro = lambda  :   0.02*np.pi*(np.random.rand(len(sol45)) - 0.5)
+Nmodulations = 50
+Ccromod45  = []
+Cdommod45  = []
+CcromodXax = []
+CdommodXax = []
+Imodoa45   = []
+ImodoaXax  = []
+for k in range(Nmodulations):
+   cd = cmddom();
+   cc = cmdcro();
+   if k == 0:
+      cd *= 0.
+      cc *= 0.
+   Cdommod45.append( Cd45( cd))
+   CdommodXax.append(CdXax(cd))
+   Ccromod45.append( Cc45( cc))
+   CcromodXax.append(CcXax(cc))
+   Imodoa45.append( Ioa45(cc))
+   ImodoaXax.append(IoaXax(cc))
 
 
 
 
 
 #%%
+CostCross45 = [];  CostCrossXax = []  # total cross intensities in dark hole
+IoffAxXax = []; IoffAx45  = [] # peak off-axis intensities
+for k in range(len(sols45)):
+   CostCross45.append( np.sum(A.DMcmd2Intensity(sols45[k ], 'cross', pixlist=pl45 )))
+   IoffAx45.append(np.max(A.DMcmd2Intensity(sols45[k], pmat='dom' ,pixlist=pl45, OffAxPhasor=Phasor45)))
+for k in range(len(solsXax)):
+   CostCrossXax.append(np.sum(A.DMcmd2Intensity(solsXax[k], 'cross', pixlist=plXax)))
+   IoffAxXax.append(np.max(A.DMcmd2Intensity(solsXax[k],pmat='dom',pixlist=plXax,OffAxPhasor=PhasorXax)))
+
 Idh45   = A.DMcmd2Intensity(sols45[-1],'dom').reshape((256,256));
 Ioa45dh  = A.DMcmd2Intensity(sols45[-1],pmat='dom',OffAxPhasor=Phasor45).reshape((256,256));
 IdhXax  = A.DMcmd2Intensity(solsXax[-1],'dom').reshape((256,256));
