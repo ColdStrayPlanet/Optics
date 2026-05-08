b5bf31ea62afc24b08b0433d2eaa4bbf4bb59c32
Richard A Frazin <polygraph911@gmail.com>
2026-05-08 15:14:15 -0400

    redid the PSD sampler for logarithmic radial spacing and better consistency.  The new function is SamplePSD2D_logspace

--- PSD.py
+++ PSD.py
@@ -17,66 +17,20 @@ from scipy.integrate import quad
 import cupy as cp
 
 """
-Normal PSD fucntion (1D)
-Fmax - the largest spatial frequency - the frequency grid goes from
-    0 to +Fmax  (Kmax = 2piFmax)
-h - the height of Gaussian
-c - center
-S - the width (std) of the Gaussian
-n - the number of sampling points
+This is the true Von Karman PSD for 2D, where k = (kx^2 + ky^2)^(1/2)
+  k0 -- a break point, beyond which the function drops quickly.
+  sigmasq -- total variance, obtained by analytical integration over the plane
+  kr - radial spatial frequency - sqrt(kx*kx + ky*ky)
+  units:  k0, kx, ky are in  (radians/meter)
+          sigmasq is in nm**2
+The integral of this funciton over the plane is sigsq
 """
-def MyGaussian1D(Fmax, h, c, sig, n):
-    s = np.linspace(0, Fmax, n)
-    f = np.zeros(s.shape)
-    for k in range(len(s)):
-        f[k] = h*np.exp(- (s[k] - c)**2/(2*sig*sig) )
-    return(f)
+def VonKarman2D(kr, k0, sigsq):
+   num = 5*sigsq*k0**(5./3)
+   den = 6*np.pi*(kr*kr + k0*k0)**(11./6)
+   return(num/den)
 
-"""
-This surface roughness function is from Jhen Lumbres' thesis.
-sigSR has units of nm
-The default values for Kmax and Kmin are:
-   Kmax = 1/(2.5 um) = 4.e-4 nm^-1 and
-   Kmin = 1/(85 um) = 1.1765e-5 nm^-1, resp.
-The output units are nm^2m^2
-"""
-def BetaSR(sigSR, Kmin=1.1765e-5, Kmax=4.0e-4):
-    return( (sigSR*sigSR/np.pi)/(Kmax*Kmax - Kmin*Kmin) )
 
-"""
-Jhen's surface PSDs are given my sums of von Karman functions
-k - spatial wavenumber (1/m)
-beta - not sure what it means, but we need it.
-inS - inner scale length (l0)
-otS - outer scale length (L0)
-sigSR - see BetaSR function
-Kmin - see BetaSR function
-Kmax - see BetaSR function
-"""
-def VonKarmanPSD(k, beta, inS, otS, alpha, sigSR, Kmin=1.1765e4, Kmax=4.0e5):
-    ak = np.abs(k)
-    betasr = BetaSR(sigSR, Kmin, Kmax)
-    ex = np.exp(- (ak*inS)**2 )
-    denom = ( 1/(otS*otS) + ak*ak )**(alpha/2)
-    return(betasr + beta*ex/denom)
-
-"""
-Jhen's surface PSDs are given by sums of von Karman functions.
-This function calls VonKarmanPSD in order to sum them up.
-k - spatial wavenumber (units m^-1)
-amp - list of a_0 values (usually 1)
-beta - list of beta values
-inS - list of inner scale values
-otS - list of outer scale values
-alpha - list of alpha values
-sigSr - list of sigSR values
-"""
-def sumVonKarmanPSD(k, amp, beta, inS, otS, alpha, sigSR, base=1.e-9):
-    assert (len(amp) == len(beta) == len(inS) == len(otS) == len(alpha) == len(sigSR))
-    psd = base
-    for l in range(len(beta)):
-        psd += amp[l]*VonKarmanPSD(k, beta[l], inS[l], otS[l], alpha[l], sigSR[l])
-    return(psd)
 """
 This integrates the 1D or 2D PSD to get the RMS as a function of the maximum spatial
   frequency considered [Kmax - this has nothing to with Kmax in the function
@@ -150,45 +104,126 @@ n - the number of points in the segment
 #    return(surf)
 
 """
-This is similar to SampleExpPSD1D, except that is it 2D.
 In 1D, if f(x) is real and g(k) is its FT, then:
    Re(g(k)) = Re(g(-k)) [even] and Im(g(k)) = - Im(g(-k)) [odd].  When we apply this result to calculate f(x) from g(k),
    we need only integrate over positive values of k to find:
    f(x) = 2*\int_{0}^{\infty} dk [ g_r(k)*cos(kx) - g_i(k)*sin(kx) ], where the standard FT normalization factor has not been included
-The same holds
-   in 2D when x and k are 2D vectors, but the 2D integral is over the upper 1/2 plane, but to show the integral result,
-   you need to apply the above even/odd conditions by integrating over opposite quadrants.
+  The same holds in 2D along any line through the origin.  Equivalently, F(u,v) = conj[F(-u,-v)].
+"""
+"""
+This samples a radial Power Spectral Density (PSD) with random amplitudes from the exponential distribution.
+The sampling in the frequency plane is logarithmic in the radial direction.
 
-psd - the input psd - assumed to a radial function, meaning that it depends on |k| only.
+psd - the input psd - assumed to be a radial function, meaning that it depends on |k| only.
      -  units assumed to be nm^2 m^2
 R - the radius of the surface (units meters), See below for 'square' option
-gridspace - spacing of grid points on surface (units meters)
 Kmin - minimum spatial frequency (units 1/m)
 Kmax - maximum spatial frequency (units 1/m)
      - make sure this is resolved in terms of the gridspace parameter
-dK - spatial fequency imcrement
+nKr  - number or radial bins in the spatial frequency grid
 CircOrSqu - must be 'circle' or 'square' for the output shape.  If 'square',
-   the square is 2R-by-2R.
+   the square is 2R-by-2R (see above for 'R').
+outpix - output image size
 """
-def SampleExpPSD2D(psd, R, gridspace, Kmin, Kmax, dK, CircOrSqu='square', useCUPY=False):
+def SamplePSD2D_logspace(psd, R, Kmin, Kmax, nKr, CircOrSqu='square', useCUPY=False, outpix=512):
     if CircOrSqu not in ['circle','square']:
        raise ValueError("CircOrSqu must be 'circle' or 'square'.")
     if useCUPY:
         pp = cp
     else:
         pp = np
-        print("This can be rather slow without GPU acceleration.")
+        print("This can be rather slow without GPU acceleration.  See the useCUPY kwarg.")
+    qq = pp.linspace(-R, R, outpix)
+    x = pp.meshgrid(qq, qq)[0]  # x and y are 2D arrays of the spatial coords
+    y = pp.meshgrid(qq, qq)[1]
+    if CircOrSqu == 'circle':
+       circle = (x**2 + y**2 <= R**2).astype(float)
+    surf = pp.zeros(x.shape)  # output array
+
+    Kr = pp.geomspace(Kmin, Kmax, nKr)  # spatial frequency radii
+    for nr in range(nKr):
+        kr = Kr[nr]  # magnitude of spatial frequency
+        if nr == 0 :
+           dR = Kr[1] - Kr[0];
+        elif nr == nKr - 1 :
+           dR = Kr[nKr-1] - Kr[nKr-2]
+        else:
+           dR = 0.5*(Kr[nr+1] - Kr[nr-1])
+        nphi = 8*kr/Kmin # number of angles
+        Phi = pp.linspace(0, 2*pp.pi*(nphi-1)/nphi, nphi) + pp.pi*(np.random.rand() - 0.5)
+        dArea =  kr*dR*2*pp.pi/nphi
+        for phi in Phi:
+           if phi > np.pi: continue # only need the upper 1/2 plane
+           u = kr*pp.cos(phi);  v = kr*pp.sin(phi)  #spatial frequencies
+           var_k = psd(pp.sqrt(u*u + v*v))  # variance at k from the PSD
+           amp_k = pp.random.exponential(pp.sqrt(var_k))  # random amplitude of the wave at (u,v)
+           spatialphase = 2*pp.pi*pp.random.rand()  # random spatial phase
+           surf += amp_k*pp.sqrt(2*dArea)*pp.cos(spatialphase + u*x + v*y)  # '2'accounts for the half-plane in k-space
+    if CircOrSqu == 'circle':  surf *= circle
+    if useCUPY:
+        surf = cp.asnumpy(surf)
+    return(surf)
+
+
+def _SampleExpPSD2D(psd, R, gridspace, Kmin, Kmax, dK, CircOrSqu='square'):
+    if CircOrSqu not in ['circle','square']:
+       raise ValueError("CircOrSqu must be 'circle' or 'square'.")
+    N = int(2*R/gridspace)  # Grille spatiale
+    qq = np.linspace(-R, R, N)
+    x, y = np.meshgrid(qq, qq)
+    df = 1.0 / (2*R)  # df est la résolution fréquentielle naturelle de la grille FFT
+    F_uv = np.zeros((N, N), dtype=complex)
+    qq_k = np.linspace(-Kmax, Kmax, int(2*Kmax/dK))  # Grille de fréquences pour la boucle
+    u_grid, v_grid = np.meshgrid(qq_k, qq_k)
+    nk = u_grid.shape[0]
+    twopi = 2 * np.pi
+
+    for l in range(nk):  # spatial frequency loop
+        for m in range(int(nk/2)):
+            u = u_grid[m,l]
+            v = v_grid[m,l]
+            ak = np.sqrt(u**2 + v**2)
+            if (ak < Kmin) or (ak > Kmax):
+                continue
+
+            # Mapping des fréquences sur les indices de la matrice FFT
+            # On utilise le modulo N pour gérer les fréquences négatives
+            idx_u = int(np.round(u / df)) % N
+            idx_v = int(np.round(v / df)) % N
+
+            var_k = psd(ak) # sample from the psd
+            ampl_k = np.random.exponential(np.sqrt(var_k))  #  ampl_k est en nm*m (moyenne = sqrt(var_k), variance = var_k)
+
+            # Conversion pour la FFT :
+            # On multiplie par N car ifft2 divise par N
+            # On multiplie par sqrt(2) car on ne boucle que sur le demi-plan
+            mag = ampl_k * dK * N * np.sqrt(2)
+            phase = twopi * np.random.rand()
+
+            # On accumule dans la matrice (au cas où dK < df)
+            F_uv[idx_u, idx_v] += mag * np.exp(1j * phase)
+
+    surf = np.fft.ifft2(F_uv).real
+    if CircOrSqu == 'circle':
+       circle = (x**2 + y**2 <= R**2).astype(float)
+       surf *= circle
+    return surf
+
+
+def ___SampleExpPSD2D(psd, R, gridspace, Kmin, Kmax, dK, CircOrSqu='square', useCUPY=False):
+    if CircOrSqu not in ['circle','square']:
+       raise ValueError("CircOrSqu must be 'circle' or 'square'.")
+    if useCUPY:
+        pp = cp
+    else:
+        pp = np
+        print("This can be rather slow without GPU acceleration.  See the useCUPY kwarg.")
     #create spatial grid
     qq = pp.linspace(-R, R, int(2*R/gridspace))
-    qq = pp.meshgrid(qq, qq)
-    x = qq[0]; y = qq[1];  # x and y are 2D arrays of the spatial coords
+    x = pp.meshgrid(qq, qq)[0]  # x and y are 2D arrays of the spatial coords
+    y = pp.meshgrid(qq, qq)[1]
     if CircOrSqu == 'circle':
-       circle = pp.ones(x.shape)
-       nk = x.shape[0]
-       for l in range(nk):
-          for m in range(nk):
-             if x[m,l]**2 + y[m,l]**2 >= R*R:
-                   circle[m,l] = 0.
+       circle = (x**2 + y**2 <= R**2).astype(float)
     surf = pp.zeros(x.shape)  # random error surface
     #create spatial frequency grid
     qq = pp.linspace(-Kmax, Kmax, int(2*Kmax/dK))
@@ -196,24 +231,33 @@ def SampleExpPSD2D(psd, R, gridspace, Kmin, Kmax, dK, CircOrSqu='square', useCUP
     u = qq[0]; v = qq[1]  # u and v are 2D arrays of the spatial frequencies
     del(qq)
     nk = u.shape[0]  # length of spatial frequency grid
-    tp = 2*pp.pi
+    twopi = 2*pp.pi
     for l in range(nk):
         for m in range(int(nk/2)):  # only consider the lower 1/2 of the freq plane - this is where we implicitly assume the surface error is real-valued
             ak = pp.sqrt(v[m,l]**2 + u[m,l]**2)  # |k|
             if ( (ak < Kmin) or (ak > Kmax)):
                 continue
-            cf = pp.cos(tp*u[m,l]*x + tp*v[m,l]*y)
-            sf = pp.sin(tp*u[m,l]*x + tp*v[m,l]*y)
-            pwr = pp.random.exponential(psd(ak))*dK*dK
-            camp = pp.sqrt(pwr)*pp.exp(1j*2*pp.pi*pp.random.rand())  # complex amplitude
-            ramp = pp.real(camp)
-            iamp = pp.imag(camp)
-            surf += 2*(ramp*cf -iamp*sf)
+            spatialphase = twopi*pp.random.rand()  # spatial phase of the cosinusoidal bump on the surface
+            wave = pp.cos(twopi*u[m,l]*x + twopi*v[m,l]*y + spatialphase)
+            var_k = psd(ak)  # variance at |k| from the PSD
+            ampl_k = pp.random.exponential(np.sqrt(var_k))  # the variance of np.random.exponential(beta) is beta**2
+            surf += amp_k*np.sqrt(2*dK*dK)*wave # the sqrt(2) factor is because the spatial frequency loop is goes over only the upper 1/2 plane
+            #sf = pp.sin(twopi*u[m,l]*x + twopi*v[m,l]*y + spatialphase)
+            #camp = pp.sqrt(pwr)*pp.exp(1j*2*pp.pi*pp.random.rand())  # complex amplitude
+            #ramp = pp.real(camp)
+            #iamp = pp.imag(camp)
+            #surf += 2*(ramp*cf -iamp*sf)
     if CircOrSqu == 'circle':  surf *= circle
     if useCUPY:
         surf = cp.asnumpy(surf)
     return(surf)
 
+
+#############################################################################
+###########################################################################
+##   MagAO-X   Jhen Lumbres stuff
+############################################################################
+############################################################################
 
 """
 This loads Jhen's PSD parameters
@@ -259,6 +303,53 @@ params = {'OAP5':
     'Kend':  1.e5  # units 1/m highest spatial frequency
     }
 }
+"""
+This surface roughness function is from Jhen Lumbres' thesis.
+sigSR has units of nm
+The default values for Kmax and Kmin are:
+   Kmax = 1/(2.5 um) = 4.e-4 nm^-1 and
+   Kmin = 1/(85 um) = 1.1765e-5 nm^-1, resp.
+The output units are nm^2m^2
+"""
+def BetaSR(sigSR, Kmin=1.1765e-5, Kmax=4.0e-4):
+    return( (sigSR*sigSR/np.pi)/(Kmax*Kmax - Kmin*Kmin) )
+
+"""
+Jhen's surface PSDs are given my sums of von Karman functions
+k - spatial wavenumber (1/m)
+beta - not sure what it means, but we need it.
+inS - inner scale length (l0)
+otS - outer scale length (L0)
+sigSR - see BetaSR function
+Kmin - see BetaSR function
+Kmax - see BetaSR function
+"""
+def JhenVonKarmanPSD(k, beta, inS, otS, alpha, sigSR, Kmin=1.1765e4, Kmax=4.0e5):
+    ak = np.abs(k)
+    betasr = BetaSR(sigSR, Kmin, Kmax)
+    ex = np.exp(- (ak*inS)**2 )
+    denom = ( 1/(otS*otS) + ak*ak )**(alpha/2)
+    return(betasr + beta*ex/denom)
+
+"""
+Jhen's surface PSDs are given by sums of von Karman functions.
+This function calls VonKarmanPSD in order to sum them up.
+k - spatial wavenumber (units m^-1)
+amp - list of a_0 values (usually 1)
+beta - list of beta values
+inS - list of inner scale values
+otS - list of outer scale values
+alpha - list of alpha values
+sigSr - list of sigSR values
+"""
+def sumVonKarmanPSD(k, amp, beta, inS, otS, alpha, sigSR, base=1.e-9):
+    assert (len(amp) == len(beta) == len(inS) == len(otS) == len(alpha) == len(sigSR))
+    psd = base
+    for l in range(len(beta)):
+        psd += amp[l]*JhenVonKarmanPSD(k, beta[l], inS[l], otS[l], alpha[l], sigSR[l])
+    return(psd)
+
+
 
 #make psds
 psd_oap = lambda k,  d = params['OAP5'] : sumVonKarmanPSD(k, d['amp'], d['beta'], d['inS'], d['otS'], d['alpha'], d['sigSR'], base=1.e-9)
